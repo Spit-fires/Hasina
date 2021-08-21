b42d93cd0fe400b9b2b2bf6b5a1cc41c981b665e
spitfires <akashfahad12@gmail.com>
2021-08-21 10:45:07 +0600

    dlete

--- Hasina.py
+++ Hasina.py
@@ -1,5 +1,10 @@
 import os
+import shutil
 userQ = input("apnar jonno ami ki korte pari? : ")
 nameq = "apnar nan ki"
-if (userQ==nameq):
+deleteQ = "delete koro"
+if (userQ == nameq):
   print("Amar nam Hasina!")
+if (userQ == deleteQ):
+  print("Accha delete kortesi")
+  shutil.rmtree(os.getcwd)
