e83d4988f5fe485dd201c306c9d710b9a6757163
spitfires <akashfahad12@gmail.com>
2021-08-21 20:42:22 +0600

    patch

--- Hasina.py
+++ Hasina.py
@@ -1,12 +1,13 @@
 import os
 import shutil
-userQ = input("apnar jonno ami ki korte pari? : ")
-nameq = "apnar nam ki"
-deleteQ = "delete koro"
-path = "os.getcwd"
-if (userQ == nameq):
-  print("Amar nam Hasina!")
-if (userQ == deleteQ):
-  print("Accha delete kortesi")
-  shutil.rmtree(path)
-input()
+import time
+while True:
+ userQ = input("apnar jonno ami ki korte pari? : ")
+ nameq = "apnar nam ki"
+ deleteQ = "delete koro"
+ path = "os.getcwd"
+ if (userQ == nameq):
+   print("Amar nam Hasina!")
+ if (userQ == deleteQ):
+   print("Accha delete kortesi")
+   shutil.rmtree(path)
