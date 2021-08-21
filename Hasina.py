822d4c13ee9b88b080a5ac5b22661562d4e975cd
spitfires <akashfahad12@gmail.com>
2021-08-21 10:47:52 +0600

    eror

--- Hasina.py
+++ Hasina.py
@@ -1,10 +1,11 @@
 import os
 import shutil
 userQ = input("apnar jonno ami ki korte pari? : ")
-nameq = "apnar nan ki"
+nameq = "apnar nam ki"
 deleteQ = "delete koro"
+path = "os.getcwd"
 if (userQ == nameq):
   print("Amar nam Hasina!")
 if (userQ == deleteQ):
   print("Accha delete kortesi")
-  shutil.rmtree(os.getcwd)
+  shutil.rmtree(path)
