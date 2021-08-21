8e1e79a92500f5742587e97260924539e1de5aea
spitfires <akashfahad12@gmail.com>
2021-08-21 20:47:21 +0600

    vars

--- Hasina.py
+++ Hasina.py
@@ -1,13 +1,10 @@
 import os
 import shutil
 import time
+import questions
 while True:
  userQ = input("apnar jonno ami ki korte pari? : ")
- nameq = "apnar nam ki"
- deleteQ = "delete koro"
- path = "os.getcwd"
  if (userQ == nameq):
    print("Amar nam Hasina!")
- if (userQ == deleteQ):
-   print("Accha delete kortesi")
-   shutil.rmtree(path)
+ if (userQ == feelingQ):
+   print("Valo asi!")
