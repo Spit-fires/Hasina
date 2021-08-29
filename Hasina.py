0ab7a304c6b65582bcab0aeda588b7c6ea2b7dc3
spitfires <akashfahad12@gmail.com>
2021-08-29 20:21:06 +0600

    added comments

--- Hasina.py
+++ Hasina.py
@@ -1,9 +1,17 @@
+# importing liberies
 import os
 import shutil
 import time
-from questions import *
-while True:
- userQ = input("apnar jonno ami ki korte pari? : ")
+# question vars
+nameQ = "apnar nam ki"
+feelingQ = "kemon asen"
+
+# functions 
+
+
+while True: # while loop to avoid exiting 
+ # Questions
+ userQ = input("apnar jonno ami ki korte pari? : ") # user question
  if (userQ == nameq):
    print("Amar nam Hasina!")
  if (userQ == feelingQ):
